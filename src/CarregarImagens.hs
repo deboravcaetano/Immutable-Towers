692d78c86499c43e646572604e696f8efbcb236c
a112332 <a112332@alunos.uminho.pt>
2025-07-17 15:55:23 +0100

    adicionar assets vida

--- src/CarregarImagens.hs
+++ src/CarregarImagens.hs
@@ -88,11 +88,58 @@ carregarImgTorreFogo = loadBMP "assets/Torres/TorreFogo.bmp"
 
 carregarImgsFlora :: IO [[Picture]]
 carregarImgsFlora = do
-    imgEsq <- loadBMP "assets/Personagens/Flora/FloraEsquerda.bmp" 
-    imgDir  <- loadBMP "assets/Personagens/Flora/FloraDireita.bmp"
-    imgN    <- loadBMP "assets/Personagens/Flora/FloraTras.bmp"
-    imgS      <- loadBMP "assets/Personagens/Flora/FloraFrente.bmp"
-    return [[imgEsq], [imgDir], [imgN], [imgS]]
+    imgE100      <- loadBMP "assets/Personagens/Flora/FloraEsquerda100.bmp"
+    imgE90      <- loadBMP "assets/Personagens/Flora/FloraEsquerda90.bmp"
+    imgE80      <- loadBMP "assets/Personagens/Flora/FloraEsquerda80.bmp"
+    imgE70      <- loadBMP "assets/Personagens/Flora/FloraEsquerda70.bmp"
+    imgE60      <- loadBMP "assets/Personagens/Flora/FloraEsquerda60.bmp"
+    imgE50      <- loadBMP "assets/Personagens/Flora/FloraEsquerda50.bmp"
+    imgE40      <- loadBMP "assets/Personagens/Flora/FloraEsquerda40.bmp"
+    imgE30      <- loadBMP "assets/Personagens/Flora/FloraEsquerda30.bmp"
+    imgE20      <- loadBMP "assets/Personagens/Flora/FloraEsquerda20.bmp"
+    imgE10      <- loadBMP "assets/Personagens/Flora/FloraEsquerda10.bmp"
+    imgE0      <- loadBMP "assets/Personagens/Flora/FloraEsquerda0.bmp"
+
+    imgD100      <- loadBMP "assets/Personagens/Flora/FloraDireita100.bmp"
+    imgD90      <- loadBMP "assets/Personagens/Flora/FloraDireita90.bmp"
+    imgD80      <- loadBMP "assets/Personagens/Flora/FloraDireita80.bmp"
+    imgD70      <- loadBMP "assets/Personagens/Flora/FloraDireita70.bmp"
+    imgD60      <- loadBMP "assets/Personagens/Flora/FloraDireita60.bmp"
+    imgD50      <- loadBMP "assets/Personagens/Flora/FloraDireita50.bmp"
+    imgD40      <- loadBMP "assets/Personagens/Flora/FloraDireita40.bmp"
+    imgD30      <- loadBMP "assets/Personagens/Flora/FloraDireita30.bmp"
+    imgD20      <- loadBMP "assets/Personagens/Flora/FloraDireita20.bmp"
+    imgD10      <- loadBMP "assets/Personagens/Flora/FloraDireita10.bmp"
+    imgD0      <- loadBMP "assets/Personagens/Flora/FloraDireita0.bmp"
+
+    imgN100      <- loadBMP "assets/Personagens/Flora/FloraTras100.bmp"
+    imgN90      <- loadBMP "assets/Personagens/Flora/FloraTras90.bmp"
+    imgN80      <- loadBMP "assets/Personagens/Flora/FloraTras80.bmp"
+    imgN70      <- loadBMP "assets/Personagens/Flora/FloraTras70.bmp"
+    imgN60      <- loadBMP "assets/Personagens/Flora/FloraTras60.bmp"
+    imgN50      <- loadBMP "assets/Personagens/Flora/FloraTras50.bmp"
+    imgN40      <- loadBMP "assets/Personagens/Flora/FloraTras40.bmp"
+    imgN30      <- loadBMP "assets/Personagens/Flora/FloraTras30.bmp"
+    imgN20      <- loadBMP "assets/Personagens/Flora/FloraTras20.bmp"
+    imgN10      <- loadBMP "assets/Personagens/Flora/FloraTras10.bmp"
+    imgN0      <- loadBMP "assets/Personagens/Flora/FloraTras0.bmp"
+
+    imgS100      <- loadBMP "assets/Personagens/Flora/FloraFrente100.bmp"
+    imgS90      <- loadBMP "assets/Personagens/Flora/FloraFrente90.bmp"
+    imgS80      <- loadBMP "assets/Personagens/Flora/FloraFrente80.bmp"
+    imgS70      <- loadBMP "assets/Personagens/Flora/FloraFrente70.bmp"
+    imgS60      <- loadBMP "assets/Personagens/Flora/FloraFrente60.bmp"
+    imgS50      <- loadBMP "assets/Personagens/Flora/FloraFrente50.bmp"
+    imgS40      <- loadBMP "assets/Personagens/Flora/FloraFrente40.bmp"
+    imgS30      <- loadBMP "assets/Personagens/Flora/FloraFrente30.bmp"
+    imgS20      <- loadBMP "assets/Personagens/Flora/FloraFrente20.bmp"
+    imgS10      <- loadBMP "assets/Personagens/Flora/FloraFrente10.bmp"
+    imgS0      <- loadBMP "assets/Personagens/Flora/FloraFrente0.bmp"
+
+    return [[imgE100,imgE90,imgE80,imgE70,imgE60,imgE50,imgE40,imgE30,imgE20,imgE10,imgE0], 
+            [imgD100,imgD90,imgD80,imgD70,imgD60,imgD50,imgD40,imgD30,imgD20,imgD10,imgD0], 
+            [imgN100,imgN90,imgN80,imgN70,imgN60,imgN50,imgN40,imgN30,imgN20,imgN10,imgN0], 
+            [imgS100,imgS90,imgS80,imgS70,imgS60,imgS50,imgS40,imgS30,imgS20,imgS10,imgS0]]
 
 carregarImgsStella :: IO [[Picture]]
 carregarImgsStella = do
